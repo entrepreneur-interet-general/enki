03c274776ab84eaad53ca1e0518f748ca2f9a8e2
Jérôme Burkard <burkardjerome@gmail.com>
2020-10-07 15:27:26 +0200

    make develop base branch for nx

--- fronts/apps/enki/jest.config.js
+++ fronts/apps/enki/jest.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  displayName: 'enki',
-  preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  displayName: "enki",
+  preset: "../../jest.preset.js",
+  // setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.(html|svg)$",
       astTransformers: {
         before: [
-          'jest-preset-angular/build/InlineFilesTransformer',
-          'jest-preset-angular/build/StripStylesTransformer',
+          "jest-preset-angular/build/InlineFilesTransformer",
+          "jest-preset-angular/build/StripStylesTransformer",
         ],
       },
     },
   },
-  coverageDirectory: '../../coverage/apps/enki',
+  coverageDirectory: "../../coverage/apps/enki",
   snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
+    "jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js",
+    "jest-preset-angular/build/AngularSnapshotSerializer.js",
+    "jest-preset-angular/build/HTMLCommentSerializer.js",
   ],
 };

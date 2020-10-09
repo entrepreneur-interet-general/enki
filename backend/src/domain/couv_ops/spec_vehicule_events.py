8653dad35375cc54d05e43fd6314623c16a436a0
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2020-10-09 18:06:42 +0200

    [WIP] specifications of intervention and vehicule events

--- backend/src/domain/couv_ops/spec_vehicule_events.py
+++ backend/src/domain/couv_ops/spec_vehicule_events.py
@@ -1,15 +1,16 @@
 from typing import Literal, TypedDict, Union
 
 from pandas import Timestamp
-vehicule_unavailable_status_options = [
+vehicule_status_options = [
     'departed_to_intervention',  
     'arrived_on_intervention', 
     'transport_to_hospital',
     'arrived_at_hospital', 
     'left_hospital', 
+    'arrived_at_home' ,
     'retrievable_within_15_minutes', # sport 
     'selected',  # présentation spontannée, sélection / instance de départ
-    'waiting',  
+    'pending',  
     'broken',
     'lacks_staff', # manque personnel, omnibus
     'cancelled',
@@ -17,15 +18,11 @@ vehicule_unavailable_status_options = [
     'realocated', # monté en garde
     'undefined', # or unused
     'misc_unavailable'    
-    ]
-vehicule_available_status_options = [
-    'arrived_at_home' ,
     'misc_available'
     ]
-vehicule_status_options = vehicule_unavailable_status_options + vehicule_available_status_options
 VehiculeStatus = Literal[vehicule_status_options]
 
-vehicule_role_options = ['victim_rescue', 'pump', 'other']∏
+vehicule_role_options = ['victim_rescue', 'pump', 'other']
 VehiculeRole = Literal[vehicule_role_options]
 
 class VehiculeEvent(TypedDict):

09b6b724ce4dab412977047b14911d90f8ba387b
rcourivaud <r.courivaiud@gmail.com>
2020-10-22 18:16:34 +0200

    udapte e2e tests with new tasks path and repositories
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- backend/tests/e2e/test_task_api.py
+++ backend/tests/e2e/test_task_api.py
@@ -4,47 +4,51 @@ import json
 
 from sqlalchemy import create_engine
 from sqlalchemy.sql.schema import MetaData
-from entrypoints.flask_app import app, repositories
+from entrypoints.flask_app import app
+from entrypoints.extensions import repositories
+
+BASE_PATH_TASK: str = "/api/enki/v1/tasks"
 
 def test_hello_sapeurs_returns_200_and_expected_message():
-  with app.test_client() as client:
-    response = client.get('/')
-    body = json.loads(response.data)
-    print("response data : ", json.loads(response.data))
-    assert response.status_code == 200
-    assert body['message'] == "Hello, Sapeurs!"
+    with app.test_client() as client:
+        response = client.get('/')
+        body = json.loads(response.data)
+        print("response data : ", json.loads(response.data))
+        assert response.status_code == 200
+        assert body['message'] == "Hello, Sapeurs!"
+
 
 def test_add_task_then_recovers_it_and_recovers_all():
-  with app.test_client() as client:
-    client: FlaskClient
-    
-    # adding a task
-    task1 = {
-      "uuid": "uuid_1",
-      "title": "My e2e title"
-    }
-    add_task_response = post_add_task(client, task1)
+    with app.test_client() as client:
+        client: FlaskClient
 
-    assert add_task_response.status_code == 201
-    assert add_task_response.json['message'] == "Success"
+        # adding a task
+        task1 = {
+            "uuid": "uuid_1",
+            "title": "My e2e title"
+        }
+        add_task_response = post_add_task(client, task1)
 
-    # fetching added task
-    fetched_task1_response = client.get("/tasks/" + task1["uuid"])
-    assert fetched_task1_response.status_code == 200
-    assert fetched_task1_response.json == task1
-    
-    # adding extra task
-    task2 = {
-      "uuid": "uuid_2",
-      "title": "My other e2e title"
-    }
-    post_add_task(client, task2)
+        assert add_task_response.status_code == 201
+        assert add_task_response.json['message'] == "Success"
 
-    # fetching all tasks
-    fetched_all_tasks_response = client.get("/tasks")
-    assert fetched_all_tasks_response.status_code == 200
-    assert fetched_all_tasks_response.json == [task1, task2]
+        # fetching added task
+        fetched_task1_response = client.get(BASE_PATH_TASK + "/" + task1["uuid"])
+        assert fetched_task1_response.status_code == 200
+        assert fetched_task1_response.json == task1
+
+        # adding extra task
+        task2 = {
+            "uuid": "uuid_2",
+            "title": "My other e2e title"
+        }
+        post_add_task(client, task2)
+
+        # fetching all tasks
+        fetched_all_tasks_response = client.get(BASE_PATH_TASK)
+        assert fetched_all_tasks_response.status_code == 200
+        assert fetched_all_tasks_response.json == [task1, task2]
+
 
 def post_add_task(client: FlaskClient, body: Dict[str, str]):
-  return client.post('/tasks', json=body)
-
+    return client.post('/tasks', json=body)

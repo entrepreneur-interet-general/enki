d28d7e89b6bf2a292d2a685b0862d2816090ad9a
rcourivaud <r.courivaiud@gmail.com>
2020-10-27 12:45:22 +0100

    update tests
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- cisu/tests/unit/test_factories.py
+++ cisu/tests/unit/test_factories.py
@@ -1,3 +1,4 @@
+from cisu.src.factories.alert_code_factory import AlertCodeFactory
 from cisu.src.factories.alert_factory import AlertFactory
 
 
@@ -5,3 +6,9 @@ def test_alert_factory():
     alert = AlertFactory().build()
     alert2 = AlertFactory().build()
     assert alert.alertId != alert2.alertId
+
+
+def test_alert_code_factory():
+    alert = AlertCodeFactory().build()
+    alert2 = AlertCodeFactory().build()
+    assert alert.riskThreat != alert2.riskThreat

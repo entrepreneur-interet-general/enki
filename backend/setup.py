f5aa52fb3b1f59e4ec3bc5c7b356b56f1b35735e
rcourivaud <r.courivaiud@gmail.com>
2020-10-28 14:34:26 +0100

    update structure
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- backend/setup.py
+++ backend/setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sapeur-api',
     version='0.1',
-    packages=['.'],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
 )

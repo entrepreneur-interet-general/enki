4bc2c01ce8cc41122a79764199fc57aa4a184ee1
Benjamin PERNEY <benjamin.perney@etam.fr>
2020-11-09 16:18:47 +0100

    Update env vars

--- fronts/apps/enki/src/app/in-memory-data.service.ts
+++ fronts/apps/enki/src/app/in-memory-data.service.ts
@@ -17,7 +17,7 @@ export class InMemoryDataService implements InMemoryDbService {
   // the method below returns the initial number (11).
   // if the heroes array is not empty, the method below returns the highest
   // hero id + 1.
-  genId(heroes: any): number {
-    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  genId(interventions: any): number {
+    return interventions.length > 0 ? Math.max(...interventions.map(hero => hero.id)) + 1 : 11;
   }
 }

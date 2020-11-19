92d7ae1a210e570bd6addfd8633c90fa8bfda7f6
Benjamin PERNEY <benjamin.perney@etam.fr>
2020-11-19 17:32:17 +0100

    Modify content type of request header

--- backend/src/entrypoints/views/echanges/resources/echanges.py
+++ backend/src/entrypoints/views/echanges/resources/echanges.py
@@ -14,10 +14,14 @@ class EchangeMessageResource(Resource):
     """
 
     def post(self):
-        if request.headers['Content-Type'] == 'text/xml':
-            current_app.logger.info("receive post message")
+        if request.headers['Content-Type'] in ["application/xml", 'text/xml']:
+            current_app.logger.info("post message")
             xml = request.data.decode("utf-8")
             AffairService.add_affair(xml, repo=current_app.context.affair)
-        return {
+            return {
                    "msg": "success"
                }, 200
+        return {
+            "msg": "error",
+            "contentType": request.headers['Content-Type']
+        }, 200

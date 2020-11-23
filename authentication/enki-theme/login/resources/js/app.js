f664eb7abf8fbda04e62e1469c5fe86c6e12b23a
Benjamin PERNEY <benjperney@gmail.com>
2020-11-23 11:19:30 +0100

    Added condition to app.js keycloak

--- authentication/enki-theme/login/resources/js/app.js
+++ authentication/enki-theme/login/resources/js/app.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var loginSubmit = document.querySelector('#kc-login');
   var showPwd = document.querySelector('.js-showPwd');
   Array.prototype.forEach.call(loginInputs, function(loginInput) {
+    if (!loginInput) return;
     loginInput.addEventListener('keyup', function() {
       if (usernameInput.value !== '' && passwordInput.value !== '') {
         loginSubmit.disabled = false;
@@ -13,13 +14,16 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     })
   })
-  showPwd.addEventListener('click', function() {
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text"
-      showPwd.classList.add('-active')
-    } else {
-      passwordInput.type = "password"
-      showPwd.classList.remove('-active')
-    }
-  })
+  if (showPwd) {
+    showPwd.addEventListener('click', function() {
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text"
+        showPwd.classList.add('-active')
+      } else {
+        passwordInput.type = "password"
+        showPwd.classList.remove('-active')
+      }
+    })
+  }
+
 });

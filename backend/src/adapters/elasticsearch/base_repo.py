31997614448aa9afe340af76789b27173bec7a81
Raphaël COURIVAUD <r.courivaud@gmail.com>
2020-12-01 18:11:39 +0100

    update provision script
    Signed-off-by: Raphaël COURIVAUD <r.courivaud@gmail.com>

--- backend/src/adapters/elasticsearch/base_repo.py
+++ backend/src/adapters/elasticsearch/base_repo.py
@@ -1,8 +1,10 @@
 import abc
+import pathlib
 from http.client import HTTPException
 
 from typing import List, Any
 from elasticsearch import Elasticsearch
+import json
 
 
 class NotFoundException(HTTPException):
@@ -13,6 +15,14 @@ class ElasticRepositoryMixin(abc.ABC):
     def __init__(self, client: Elasticsearch, index_name: str):
         self.client: Elasticsearch = client
         self.index_name: str = index_name
+        self.create_indice()
+
+    def create_indice(self):
+        if not self.client.indices.exists(index=self.index_name):
+            template_path = pathlib.Path(pathlib.Path(__file__).parent.absolute()) / f"templates/{self.index_name}.json"
+            with template_path.open() as f:
+                template = json.load(f)
+            self.client.indices.create(self.index_name, body=template)
 
     def __add(self, doc: dict, uuid: str) -> bool:
         return self.client.index(index=self.index_name, id=uuid, body=doc)

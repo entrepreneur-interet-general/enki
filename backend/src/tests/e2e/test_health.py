048a45bdbeb5bc79cf900ef16518038b1a433d3e
rcourivaud <r.courivaiud@gmail.com>
2020-12-02 14:53:29 +0100

    update tests
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- backend/src/tests/e2e/test_health.py
+++ backend/src/tests/e2e/test_health.py
@@ -11,3 +11,16 @@ def test_hello_sapeurs_returns_200_and_expected_message(client: FlaskClient):
     print("response data : ", json.loads(response.data))
     assert response.status_code == 200
     assert body['message'] == "Hello, Sapeurs!"
+
+def test_hello_enki_returns_200_and_expected_message(client: FlaskClient):
+    response = client.get('/api/enki/')
+    body = json.loads(response.data)
+    print("response data : ", json.loads(response.data))
+    assert response.status_code == 200
+    assert body['message'] == "Hello, Enki!"
+
+def test_hello_enki_returns_404(client: FlaskClient):
+    response = client.get('/api/enki/v1')
+    body = json.loads(response.data)
+    print("response data : ", json.loads(response.data))
+    assert response.status_code == 404

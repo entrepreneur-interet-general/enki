b89b9c27390752157b53ba286fd0ba401091ca71
Benjamin PERNEY <benjperney@gmail.com>
2020-12-10 15:45:44 +0100

    Add user service + get code insee to get affairs

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './app-auth-guard.service';
 import { GuardRegisterGuard } from './guards/guard-register.guard';
-// import { AppComponent } from './app.component';
 import { ListeInterventionsComponent } from './interventions/liste/liste-interventions.component';
 import { DetailInterventionComponent } from './interventions/detail/detail-intervention.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard/user-dashboard.component';

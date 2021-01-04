a8947ac67d210112895b25a8edbc6f5f09f3c4fb
rcourivaud <r.courivaiud@gmail.com>
2021-01-04 14:10:25 +0100

    fix imports
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- backend/src/domain/tasks/entities/info_entity.py
+++ backend/src/domain/tasks/entities/info_entity.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json
+from typing import List
 
 from domain.tasks.entities.message_entity import MessageEventEntity
 

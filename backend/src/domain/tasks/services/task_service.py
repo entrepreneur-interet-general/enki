5560aac109bb223928393ca656d26f51f3452e53
Raphaël COURIVAUD <r.courivaud@gmail.com>
2021-01-06 14:28:00 +0100

    add TaskSchema in Task service
    Signed-off-by: Raphaël COURIVAUD <r.courivaud@gmail.com>

--- backend/src/domain/tasks/services/task_service.py
+++ backend/src/domain/tasks/services/task_service.py
@@ -1,10 +1,13 @@
 from typing import Any, Dict, List
 from domain.tasks.entities.tag_entity import TagEntity
 from domain.tasks.entities.task_entity import TaskEntity
+from domain.tasks.schema import TaskSchema, TagSchema
 from service_layer.unit_of_work import AbstractUnitOfWork
 
 
 class TaskService:
+    schema = TaskSchema
+
     @staticmethod
     def add_task(uuid: str, title: str, description: str, uow: AbstractUnitOfWork):
         new_task = TaskEntity(uuid=uuid, title=title, description=description)
@@ -25,23 +28,22 @@ class TaskService:
     def list_tags(uuid: str, uow: AbstractUnitOfWork) -> List[Dict[str, Any]]:
         with uow:
             task: TaskEntity = uow.task.get_tags(uuid)
-            return [tag.to_dict() for tag in task.tags]
+            return TagSchema(many=True).dump(task.tags)
 
     @staticmethod
     def get_task_tag(uuid: str, tag_uuid: str, uow: AbstractUnitOfWork) -> Dict[str, Any]:
         with uow:
             tag: TagEntity = uow.task.get_tag_by_task(uuid=uuid, tag_uuid=tag_uuid)
-            return tag.to_dict()
+            return TagSchema().dump(tag)
 
     @staticmethod
     def list_tasks(uow: AbstractUnitOfWork) -> List[Dict[str, Any]]:
         with uow:
             tasks: List[TaskEntity] = uow.task.get_all()
-            serialized_tasks = [task.to_dict() for task in tasks]
-            return serialized_tasks
+            return TaskService.schema(many=True).dump(tasks)
 
     @staticmethod
     def get_by_uuid(uuid: str, uow: AbstractUnitOfWork) -> Dict[str, Any]:
         with uow:
             task = uow.task.get_by_uuid(uuid)
-            return task.to_dict()
+            return TaskService.schema().dump(task)

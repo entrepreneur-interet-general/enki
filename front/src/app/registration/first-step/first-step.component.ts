0a320b83562c5b186cc3f3019b1be72d2559fc44
Benjamin PERNEY <benjperney@gmail.com>
2021-02-02 13:28:57 +0100

    Prep demo

--- front/src/app/registration/first-step/first-step.component.ts
+++ front/src/app/registration/first-step/first-step.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { UserService } from '../../user/user.service'
 import { Router } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-first-step',
@@ -27,7 +28,8 @@ export class FirstStepComponent {
   constructor(
     private http: HttpClient,
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private keycloakService: KeycloakService
   ) {
     this.updateUserUrl = `http://localhost:8000/enki/v1/users`;
     this.fonctions = {
@@ -90,6 +92,15 @@ export class FirstStepComponent {
       this.userService.user.fullname = `${this.userGroup.value.firstName} ${this.userGroup.value.lastName}`
       /* this.keycloakService.updateToken(3600).then(() => {
         if(this.keycloakService.getUserRoles().includes('watchEvents')) { */
+          this.keycloakService.clearToken()
+          this.keycloakService.getToken().then((res) => {
+            console.log(res)
+            window.localStorage.setItem('token', res)
+            // let decodedJWT: any = jwt_decode(res)
+
+            // this.userService.user.attributes.fonction = decodedJWT.fonction ? decodedJWT.fonction : ""
+            // this.userService.user.attributes.code_insee = decodedJWT.code_insee ? decodedJWT.code_insee : ""
+          })
           this.router.navigate(['dashboard'])
 /*         }
       }) */

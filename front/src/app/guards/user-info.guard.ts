92ae26ee56fe0d1c4dfed3aac48accab16a9fba3
Benjamin PERNEY <benjperney@gmail.com>
2021-02-09 14:33:21 +0100

    Works like a charm

--- front/src/app/guards/user-info.guard.ts
+++ front/src/app/guards/user-info.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { UserService } from '../user/user.service';
 
 @Injectable({
@@ -9,7 +10,7 @@ import { UserService } from '../user/user.service';
 export class UserInfoGuard implements CanActivate {
   constructor(
     private userService: UserService,
-    // private router: Router
+    private router: Router
   ) {
 
   }
@@ -17,7 +18,43 @@ export class UserInfoGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       console.log(route)
-      return this.userService.userIsAuth(route);
+      return this.userIsAuth(route);
+  }
+
+  userIsAuth(routerSnapshot): Observable<boolean | UrlTree> {
+    if (this.userService.userExist) {
+      return of(this.getUrlTreeFromCurrentSnapshot(routerSnapshot))
+    } else {
+      return this.userService.getUserInfo().pipe(
+        map(res => {
+          if (res.answer === 'yes') {
+            this.userService.userExist = true
+          }
+          // let urlTreeToSend = this.userExist && routerSnapshot ? this.router.parseUrl('/dashboard'): this.router.parseUrl('/register/step1')
+          // return this.userExist ? true : urlTreeToSend
+          return this.getUrlTreeFromCurrentSnapshot(routerSnapshot)
+        }
+        // return res.answer === 'yes'
+        )
+      )
+    }
+  }
+    
+  getUrlTreeFromCurrentSnapshot(routerSnapshot) {
+    const routerSnapshotIsRegister = routerSnapshot.url.length > 0 && routerSnapshot.url[0].path === 'register' ? true : false
+    if (this.userService.userExist) {
+      if (routerSnapshotIsRegister) {
+        return this.router.parseUrl('/dashboard')
+      } else {
+        return true
+      }
+    } else {
+      if (routerSnapshotIsRegister){
+        return true
+      } else {
+        return this.router.parseUrl('/register/step1')
+      }
+    }
   }
   
 }
--- front/src/app/user/user.service.ts
+++ front/src/app/user/user.service.ts
@@ -1,11 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router, UrlTree } from '@angular/router';
-import { KeycloakService } from 'keycloak-angular';
 import { Observable, of } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { AnnuaireService } from '../annuaire/annuaire.service';
 import { Contact } from '../interfaces/Contact';
 import { User } from '../interfaces/User';
 
@@ -19,9 +16,7 @@ export class UserService {
 
   constructor(
     // private keycloakService: KeycloakService,
-    private annuaireService: AnnuaireService,
     private http: HttpClient,
-    private router: Router
   ) {
     this.user = {
       attributes: {
@@ -34,8 +29,8 @@ export class UserService {
   }
 
   getUserInfo(): Observable<any> {
-    // return this.http.get<any>(`https://run.mocky.io/v3/50538c73-1413-4066-9a0c-3ae98d63c20c`) // yes
-    return this.http.get<any>(`https://run.mocky.io/v3/9a2b30a7-e02d-4132-9943-0a972f7a4cf5`) // no
+    return this.http.get<any>(`https://run.mocky.io/v3/50538c73-1413-4066-9a0c-3ae98d63c20c`) // yes
+    // return this.http.get<any>(`https://run.mocky.io/v3/9a2b30a7-e02d-4132-9943-0a972f7a4cf5`) // no
     return of({answer: 'no'})
   }
 
@@ -82,41 +77,7 @@ export class UserService {
     return Object.keys(this.user).length > 0 && this.user.location !== '' && this.user.attributes.fonction !== '';
   }
 
-  userIsAuth(routerSnapshot): Observable<boolean | UrlTree> {
-    if (this.userExist) {
-      this.getUrlTreeFromCurrentSnapshot(routerSnapshot)
-    } else {
-      return this.getUserInfo().pipe(
-        map(res => {
-          if (res.answer === 'yes') {
-            this.userExist = true
-          }
-          // let urlTreeToSend = this.userExist && routerSnapshot ? this.router.parseUrl('/dashboard'): this.router.parseUrl('/register/step1')
-          // return this.userExist ? true : urlTreeToSend
-          return this.getUrlTreeFromCurrentSnapshot(routerSnapshot)
-        }
-        // return res.answer === 'yes'
-        )
-      )
-    }
-  }
-    
-  getUrlTreeFromCurrentSnapshot(routerSnapshot) {
-    const routerSnapshotIsRegister = routerSnapshot.url.length > 0 && routerSnapshot.url[0].path === 'register' ? true : false
-    if (this.userExist) {
-      if (routerSnapshotIsRegister) {
-        return this.router.parseUrl('/dashboard')
-      } else {
-        return true
-      }
-    } else {
-      if (routerSnapshotIsRegister){
-        return true
-      } else {
-        return this.router.parseUrl('/register/step1')
-      }
-    }
-  }
+
 
 /*   loadUserProfile(): void {
     this.keycloakService.loadUserProfile().then((response: any) => {

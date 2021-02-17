ca51785f72364f55b38b9f209c10d4f47304d238
Benjamin PERNEY <benjperney@gmail.com>
2021-02-17 12:15:47 +0100

    Working tests create maire and prefet accounts

--- e2e/test.js
+++ e2e/test.js
@@ -1,24 +1,78 @@
 import { Selector } from 'testcafe';
 
-fixture `Getting Started`
+fixture `Authentication register`
     .page `http://localhost:1337`;
 
-    const fonctionSelect = Selector('#fonction');
-    const fonctionOption = fonctionSelect.find('option');
-test('My first test', async t => {
+    const structureSelect = Selector('#structure');
+    const structureOption = structureSelect.find('option');
+    const positionSelect = Selector('#position');
+    const positionOption = positionSelect.find('option')
+    test('Create account maire', async t => {
 
-    // Test code
-    await t
-      .typeText('#username', 'mairie@chelles.fr')
-      .typeText('#password', 'azeRTY123#')
-      .click('#kc-login')
+        // Test code
+        await t
+        .click('.login-signupLink')
+        .typeText('#email', 'mairie@chelles.fr')
+        .typeText('#password', 'azeRTY123#')
+        .typeText('#password-confirm', 'azeRTY123#')
+        .click('#kc-form-buttons input')
 
-      await Selector('.userInfoForm')
-      await t
-      .typeText('#lastName', 'PERNEY')
-      .typeText('#firstName', 'Benjamin')
-      .click(fonctionSelect)
-      .click(fonctionOption.withText('Préfet'))
-      .expect(fonctionSelect.value).eql('prefet')
+        await Selector('.userInfoForm')
+        await t
+        .typeText('#lastName', 'PERNEY')
+        .typeText('#firstName', 'Benjamin')
+        .click(structureSelect)
+        .click(structureOption.withText('Mairie'))
+        .click(positionSelect)
+        .click(positionOption.withText('Maire'))
+        .click('#location')
+        .typeText('.fullscreen-form--searchInput', 'Chelles')
+        .click('#chelles')
+        .click('#submit-register')
+        
+        await Selector('.dashboard')
 
-});
+        
+    });
+
+    test('Create account prefet', async t => {
+        // Test code
+        await t
+        .click('.login-signupLink')
+        .typeText('#email', 'prefet@seineetmarne.fr')
+        .typeText('#password', 'azeRTY123#')
+        .typeText('#password-confirm', 'azeRTY123#')
+        .click('#kc-form-buttons input')
+
+        await Selector('.userInfoForm')
+        await t
+        .typeText('#lastName', 'PERNEY')
+        .typeText('#firstName', 'Benjamin')
+        .click(structureSelect)
+        .click(structureOption.withText('Prefecture'))
+        .click(positionSelect)
+        .click(positionOption.withText('Préfet'))
+        .click('#location')
+        .typeText('.fullscreen-form--searchInput', 'Seine')
+        .click('#seine-et-marne')
+        .click('#submit-register')
+        
+        await Selector('.dashboard')
+
+    })
+    
+    
+    
+    
+    /* 
+            .typeText('#username', 'mairie@chelles.fr')
+            .typeText('#password', 'azeRTY123#')
+            .click('#kc-login')
+    
+            await Selector('.userInfoForm')
+            await t
+            .typeText('#lastName', 'PERNEY')
+            .typeText('#firstName', 'Benjamin')
+            .click(fonctionSelect)
+            .click(fonctionOption.withText('Préfet'))
+            .expect(fonctionSelect.value).eql('prefet') */

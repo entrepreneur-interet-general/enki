d20c1ef3a48391702c1dd5a6915c5deb45d399ef
rcourivaud <r.courivaiud@gmail.com>
2021-02-26 11:48:53 +0100

    add script to easily test in flask shell
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- backend/scripts/test_searchable.py
+++ backend/scripts/test_searchable.py
@@ -1,9 +1,11 @@
 from sqlalchemy_searchable import search
 from flask import current_app
+from sqlalchemy import func
+
 from domain.users.entities.group import GroupEntity
 
 uow = current_app.context
 with uow:
-    query = uow.session.query(GroupEntity)
-    query = search(query, "Pré")
+    query = uow.session.query(GroupEntity).filter(GroupEntity.search_vector.op('@@')(func.plainto_tsquery('Pré')))
     print(query.all())
+    query = search(query, "Pré")

8ebd4c5f2c4771ed028dc67ab0e210391ddba95f
Benjamin PERNEY <benjperney@gmail.com>
2021-03-11 19:25:28 +0100

    To be continued

---
+++ front/src/app/search-etablissement/search-etablissement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-etablissement',
+  templateUrl: './search-etablissement.component.html',
+  styleUrls: ['./search-etablissement.component.scss']
+})
+export class SearchEtablissementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ front/src/app/search-etablissement/search-etablissement.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchEtablissementComponent } from './search-etablissement.component';
+
+
+
+@NgModule({
+  declarations: [SearchEtablissementComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SearchEtablissementModule { }

e5f247a57f4a7cf5a494f61b6e5c50b01874afbb
Benjamin PERNEY <benjperney@gmail.com>
2021-04-13 20:10:14 +0200

    HTTP request on every 4 seconds in messages

--- front/src/app/evenements/main-courante/filter-messages.pipe.ts
+++ front/src/app/evenements/main-courante/filter-messages.pipe.ts
@@ -8,13 +8,12 @@ import { Message } from './messages.service';
 export class FilterMessagesPipe implements PipeTransform {
 
   transform(messages: Message[], filter: Filter, ...args: any[]): unknown {
-
     // Établissement : messages.filter(message => message.creator.position.group_id === "61b7324b-632f-4f22-bc5e-f288117c74fe"
     // Type de message : messages.filter(message => message.type === "meeting")
     // 
     // return messages.filter(message => message.creator.position.group_id === "61b7324b-632f-4f22-bc5e-f288117c74fe")
     // return messages.filter(message => message.creator.uuid === '75d9bc1d-207f-4cc2-8d40-ef420739d128')
-    if (messages.length === 0) return [];
+    if (!messages || messages.length === 0) return [];
     const isEmpty = !Object.values(filter).some(x => (x !== null && x !== '')) ? messages : null;
 
     return isEmpty ? messages : messages.filter(message => {
--- front/src/app/evenements/main-courante/liste-main-courante/liste-main-courante.component.ts
+++ front/src/app/evenements/main-courante/liste-main-courante/liste-main-courante.component.ts
@@ -23,6 +23,8 @@ export class ListeMainCouranteComponent implements OnInit {
   user: User;
   currentEventFilter: Filter;
   exportType = new FormControl('csv');
+  messages$: Observable<Message[]>;
+  subscription: any;
 
   @ViewChild(ModalComponent) modal: ModalComponent;
 
@@ -34,21 +36,29 @@ export class ListeMainCouranteComponent implements OnInit {
     public mobilePrototype: MobilePrototypeService,
     private route: ActivatedRoute,
     ) {
-    this.messages = []
-    this.uuid = this.evenementsService.selectedEvenementUUID.getValue()
+      this.messages = []
+      this.uuid = this.evenementsService.selectedEvenementUUID.getValue()
     const event = this.evenementsService.getEvenementByID(this.uuid)
     this.currentEventFilter = event.filter
     this.user = this.userService.user
   }
 
   ngOnInit(): void {
-    this.messagesService.getMessagesByEvenementID(this.uuid).subscribe(messages => {
+
+    const timer$ = timer(0, 3000);
+    this.messages$ = timer$.pipe(
+      switchMap(() => this.messagesService.getMessagesByEvenementID(this.uuid))
+    )
+    this.subscription = this.messages$.subscribe((messages) => {
+      this.evenementsService.setMessages(this.uuid, this.messages);
+      this.fetchedMessages = true;
       this.messages = messages.sort((a, b) => {
         return new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
       });
-      this.evenementsService.setMessages(this.uuid, this.messages);
-      this.fetchedMessages = true;
     })
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
   openModal(): void {
     this.modal.open()

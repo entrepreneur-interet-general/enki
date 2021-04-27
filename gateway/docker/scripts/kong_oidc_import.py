8f3ab8c983869257c18c2151787ce893f8a8230c
Raphaël COURIVAUD <r.courivaud@gmail.com>
2021-04-27 10:49:08 +0200

    wip
    Signed-off-by: Raphaël COURIVAUD <r.courivaud@gmail.com>

--- gateway/docker/scripts/kong_oidc_import.py
+++ gateway/docker/scripts/kong_oidc_import.py
@@ -7,7 +7,7 @@ import os
 KONG_HOST_IP = os.environ.get("KONG_HOST_IP")
 KONG_PORT = os.environ.get("KONG_PORT")
 
-KONG_URL=os.environ.get("KONG_URL", f"http://{KONG_HOST_IP}:{KONG_PORT}")
+KONG_URL = os.environ.get("KONG_URL", f"http://{KONG_HOST_IP}:{KONG_PORT}")
 
 KEYCLOAK_HOST_IP = os.environ.get("KEYCLOAK_HOST_IP")
 KEYCLOAK_PORT = os.environ.get("KEYCLOAK_PORT")
@@ -20,7 +20,7 @@ CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 SGE_API_KEY = os.environ.get("SGE_API_KEY")
 REALM_NAME = "enki"
-AUTH_URI=os.environ.get("AUTH_URI", f"http://{KEYCLOAK_HOST_IP}:{KEYCLOAK_PORT}")
+AUTH_URI = os.environ.get("AUTH_URI", f"http://{KEYCLOAK_HOST_IP}:{KEYCLOAK_PORT}")
 
 ENKI_BACKEND_SERVICE_ID = os.environ.get("ENKI_BACKEND_SERVICE_ID")
 ENKI_FRONT_SERVICE_ID = os.environ.get("ENKI_FRONT_SERVICE_ID")
@@ -33,34 +33,34 @@ services = [
         'name': "enki_api",
         'url': BACKEND_URI,
 
-        "routes":[
+        "routes": [
             {
                 "path": '/api',
-                'auth':'oidc'
+                'auth': 'oidc'
             },
             {
-                'path':'/echanges',
-                'auth':'api_key'
+                'path': '/echanges',
+                'auth': 'api_key'
             }
         ]
     },
-        {
+    {
         'name': "enki_front",
         'url': FRONTEND_URI,
-        'routes':[
-             {
+        'routes': [
+            {
                 'path': '/',
-                "auth":"oidc"
-             },
+                "auth": "oidc"
+            },
         ]
     },
     {
         'name': "enki_front_test",
         'url': FRONTEND_TEST_URI,
-        'routes':[
-             {
+        'routes': [
+            {
                 'path': '/test',
-             },
+            },
         ]
     }
 ]
@@ -74,47 +74,47 @@ for _id in [e["id"] for e in response.json()["data"]]:
     reponse = requests.delete(f'{KONG_URL}/services/{_id}')
     print(response.json())
 
-    for service in services:
-        print(f"Start configuring {service['name']}")
-        response = requests.post(f'{KONG_URL}/services', data={
-            'name': service["name"],
-            'url': service["url"]
+for service in services:
+    print(f"Start configuring {service['name']}")
+    response = requests.post(f'{KONG_URL}/services', data={
+        'name': service["name"],
+        'url': service["url"]
+    })
+    created_service_id = response.json()["id"]
+    for route in service["routes"]:
+        auth_type = route.get("auth")
+
+        print(f"configuring route {route['path']}... with {auth_type}")
+        response = requests.post(f'{KONG_URL}/services/{service["name"]}/routes', data={
+            'service.id': created_service_id,
+            'paths[]': route["path"],
         })
-        created_service_id = response.json()["id"]
-        for route in service["routes"]:
-            auth_type = route.get("auth")
+        route_id = response.json()["id"]
 
-            print(f"configuring route {route['path']}... with {auth_type}")
-            response = requests.post(f'{KONG_URL}/services/{service["name"]}/routes', data={
-                'service.id': created_service_id,
-                'paths[]': route["path"],
+        if auth_type == "api_key":
+            response = requests.post(f'{KONG_URL}/routes/{route_id}/plugins', data={
+                'name': "key-auth",
+                'config.key_names': "x-api-key",
             })
-            route_id = response.json()["id"]
+            response = requests.post(f'{KONG_URL}/consumers', data={
+                'username': "sge-hub",
+            })
+            response = requests.post(f'{KONG_URL}/consumers/sge-hub/key-auth', data={
+                'key': SGE_API_KEY,
+            })
 
-            if auth_type == "api_key":
-                response = requests.post(f'{KONG_URL}/routes/{route_id}/plugins', data={
-                    'name': "key-auth",
-                    'config.key_names': "x-api-key",
-                })
-                response = requests.post(f'{KONG_URL}/consumers', data={
-                    'username': "sge-hub",
-                })
-                response = requests.post(f'{KONG_URL}/consumers/sge-hub/key-auth', data={
-                    'key': SGE_API_KEY,
-                })
+        elif auth_type == "oidc":
+            data = {
+                'name': 'oidc',
+                'config.client_id': f'{CLIENT_ID}',
+                'config.client_secret': f'{CLIENT_SECRET}',
+                'config.realm': f'{REALM_NAME}',
+                'config.bearer_only': 'false',
+                'config.introspection_endpoint': introspection_url,
+                'config.discovery': discovery_url
+            }
 
-            elif auth_type == "oidc":
-                data = {
-                    'name': 'oidc',
-                    'config.client_id': f'{CLIENT_ID}',
-                    'config.client_secret': f'{CLIENT_SECRET}',
-                    'config.realm': f'{REALM_NAME}',
-                    'config.bearer_only': 'false',
-                    'config.introspection_endpoint': introspection_url,
-                    'config.discovery': discovery_url
-                }
-
-                response = requests.post(f'{KONG_URL}/routes/{route_id}/plugins', data=data)
-            else:
-                print("No oidc")
-                pass
+            response = requests.post(f'{KONG_URL}/routes/{route_id}/plugins', data=data)
+        else:
+            print("No oidc")
+            pass

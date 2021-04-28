3cb09041ace6616d3aea12996f442b6d616845e2
Benjamin PERNEY <benjperney@gmail.com>
2021-04-28 10:32:04 +0200

    Invitation error handling

--- front/src/app/account/invitation/invitation.component.ts
+++ front/src/app/account/invitation/invitation.component.ts
@@ -2,9 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { RegisterService } from 'src/app/registration/register.service';
 import { SearchEtablissementService } from 'src/app/search-etablissement/search-etablissement.service';
+import { ToastService } from 'src/app/toast/toast.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -28,7 +30,8 @@ export class InvitationComponent implements OnInit {
     private registerService: RegisterService,
     private router: Router,
     private etablissementService: SearchEtablissementService,
-    private http: HttpClient
+    private http: HttpClient,
+    private toastService: ToastService,
   ) {
     this.registerService.getUserTypes().subscribe(response => {
       this.groupTypes = response
@@ -49,13 +52,22 @@ export class InvitationComponent implements OnInit {
       group_type: this.invitationGroup.value.group,
       group_id: this.etablissementService.selectedEtablissement.getValue().uuid
     }
-    this.httpSubmitForm(bodyForm).subscribe((response) => {
+    this.httpSubmitForm(bodyForm).subscribe(() => {
       this.invitationGroup.reset();
     })
   }
 
   httpSubmitForm(body: object): Observable<any> {
     return this.http.post<any>(`${environment.backendUrl}/invitation`, body)
+      .pipe(
+        catchError((error) => {
+          if (error.error) {
+            this.toastService.addMessage(`Status: ${error.status}
+            ${JSON.stringify(error.error.message)}`)
+          }
+          return throwError(error)
+        })
+      )
   }
 
   goToSearchEtablissement(): void {

da0bf858fa6f70e64b35a92e7818665575888a4e
rcourivaud <r.courivaiud@gmail.com>
2021-05-11 10:43:02 +0200

    fix env prod
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   prototype: true,
   production: true,
   auth: true,
-  backendUrl: `https://enki.nexsis112.fr/api`,
+  backendUrl: `https://enki.nexsis112.fr/api/enki/v1`,
   keycloakUrl: `https://auth.enki.nexsis112.fr/auth/`
 };

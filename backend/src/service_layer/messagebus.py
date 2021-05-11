d156324b6faa7dd0848f730e27c1125771f7c18d
rcourivaud <r.courivaiud@gmail.com>
2021-05-11 09:31:11 +0200

    hotfix
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- backend/src/service_layer/messagebus.py
+++ backend/src/service_layer/messagebus.py
@@ -15,7 +15,7 @@ EVENT_HANDLERS: Dict[Type[Topic], List[Callable]] = {
         events.create_message_from_meeting,
     ],
     topics.UserEventInvitationTopic: [
-         events.send_email_to_user_when_invite_to_an_event,
+         # events.send_email_to_user_when_invite_to_an_event,
      ],
 }
 
